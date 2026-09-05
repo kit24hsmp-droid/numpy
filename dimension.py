a9d64d928e1a8689fab41e67bf8503be0f4aa796
kit24hsmp-droid <kit24h.smp@ismt.edu.np>
2026-09-05 08:20:35 +0545

    This some numpy dimension

---
+++ dimension.py
@@ -0,0 +1,23 @@
+# o dismension array 
+import numpy as np
+arr = np.array([40])
+print(arr)
+print(arr.ndim)
+
+#1d array 
+import numpy as np
+arr = np.array([1, 2, 3, 4, 5]) 
+print(arr)
+print(arr.ndim)
+
+#2d array
+import numpy as np
+arr = np.array([[1,2,3],[4,5,6]])
+print(arr)
+print(arr.ndim)
+
+#3d array
+import numpy as np      
+arr = np.array([[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]])
+print(arr)
+print(arr.ndim)# this is used to find the dimension of the array
