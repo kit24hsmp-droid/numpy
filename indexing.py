6d85e66a0164fd06e25f492d3c1ac0c534e5fdc7
kit24hsmp-droid <kit24h.smp@ismt.edu.np>
2026-09-05 08:58:12 +0545

    updating indexing

---
+++ indexing.py
@@ -0,0 +1,4 @@
+import numpy as np 
+arr = np.array([[1,2,3,4,5],[6,7,8,9,10]])
+print('2nd element on 1st dim:', arr[0,1]) # this is used to find the 2nd element on 1st dimension
+print('3rd element on 2nd dim:', arr[1,2]) # this is used to find the 3rd element on 2nd dimension
