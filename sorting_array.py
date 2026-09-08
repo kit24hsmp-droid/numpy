6ca3ef4df85e49d9b0abbb13980a5c0773b65bd5
kit24hsmp-droid <kit24h.smp@ismt.edu.np>
2026-09-08 20:25:30 +0545

    order wise alphapetical order and numerical order

---
+++ sorting_array.py
@@ -0,0 +1,13 @@
+import numpy as np 
+arr = np.array([3,2,0,1])
+print(np.sort(arr)) # this is used to sort the array in ascending order
+
+#alphabetical order
+import numpy as np 
+arr = np.array(['banana', 'cherry', 'apple'])
+print(np.sort(arr))# this is used to sort the array in alphabetical order
+
+#2D array
+import numpy as np
+arr = np.array([[3,2,4],[5,0,1]])
+print(np.sort(arr)) # this is used to sort the 2D array in ascending order
